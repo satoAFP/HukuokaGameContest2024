40a015fdb02d0ad6fa55aaf9e086a0a62ef6b24b
satoAFP <72423443+satoAFP@users.noreply.github.com>
2023-07-31 14:33:06 +0900

    ログイン機能実装

--- test_net/Assets/Script/SampleScene.cs
+++ test_net/Assets/Script/SampleScene.cs
@@ -1,26 +1,44 @@
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 // MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
 public class SampleScene : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private InputField roomName;
+
+    private bool first = true;
+
     private void Start()
     {
         // プレイヤー自身の名前を"Player"に設定する
         PhotonNetwork.NickName = "Player";
 
-        // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
-        PhotonNetwork.ConnectUsingSettings();
+        
+    }
+
+    public void LogIn()
+    {
+        if (first)
+        {
+            if (roomName.text.ToString() != "")
+            {
+                // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
+                PhotonNetwork.ConnectUsingSettings();
+                first = false;
+            }
+        }
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnConnectedToMaster()
     {
-        
+        var roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
 
         // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+        PhotonNetwork.JoinOrCreateRoom(roomName.text.ToString(), roomOptions, TypedLobby.Default);
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
