8e434d374c6f2d85c6af1f2e49be201fb4065cac
MSI\GA4 <h1202019007@hiratagakuen.onmicrosoft.com>
2023-10-10 16:30:56 +0900

    liftblock製作中

--- test_net/Assets/User/Sato/Script/Gimmick/GimmickBlock.cs
+++ test_net/Assets/User/Sato/Script/Gimmick/GimmickBlock.cs
@@ -8,8 +8,12 @@ public class GimmickBlock : CGimmick
     //ÇªÇÍÇºÇÍÇÃÉ{É^Éìì¸óÕèÛãµ
     [System.NonSerialized] public bool liftMode = false;
 
+    private GameObject Player;
+
     private bool hitOwner = false;
     private bool hitClient = false;
+
+    private Vector3 dis = Vector3.zero;
 
     private bool first = true;
 
@@ -19,16 +23,35 @@ public class GimmickBlock : CGimmick
         {
             if(first)
             {
-                Vector3 input = gameObject.transform.localPosition;
+                Vector3 input = gameObject.transform.position;
                 input.y += 1;
                 gameObject.transform.localPosition = input;
+
+                dis = transform.position - Player.transform.position;
+                
+                first = false;
             }
 
+            Debug.Log("aaa");
+            gameObject.transform.position = dis + Player.transform.position;
 
+
+            liftMode = true;
         }
         else
         {
-            
+            if (!first)
+            {
+                Vector3 input = gameObject.transform.position;
+                input.y -= 1;
+                gameObject.transform.localPosition = input;
+
+                dis = new Vector3(gameObject.transform.position.x - Player.transform.position.x, gameObject.transform.position.y - Player.transform.position.y, 0);
+
+                first = true;
+            }
+
+            liftMode = false;
         }
 
 
@@ -40,25 +63,39 @@ public class GimmickBlock : CGimmick
         if (collision.gameObject.name == "Player1")
         {
             hitOwner = true;
+
+            if(PhotonNetwork.LocalPlayer.IsMasterClient)
+            {
+                Player = collision.gameObject;
+            }
         }
 
         if (collision.gameObject.name == "Player2")
         {
             hitClient = true;
+
+            if (PhotonNetwork.LocalPlayer.IsMasterClient)
+            {
+                Player = collision.gameObject;
+            }
         }
     }
 
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Player1")
+        //éùÇøè„Ç∞ÇƒÇ¢ÇÈÇ∆Ç´
+        if (!liftMode)
         {
-            hitOwner = false;
-        }
+            if (collision.gameObject.name == "Player1")
+            {
+                hitOwner = false;
+            }
 
-        if (collision.gameObject.name == "Player2")
-        {
-            hitClient = false;
+            if (collision.gameObject.name == "Player2")
+            {
+                hitClient = false;
+            }
         }
     }
 
