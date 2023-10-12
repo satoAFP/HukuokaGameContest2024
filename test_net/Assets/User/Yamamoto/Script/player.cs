5f7e0ddfbe996ba54d3eb29f88994a5774781236
yamakan-z <h1202016006@hiratagakuen.onmicrosoft.com>
2023-10-12 14:24:54 +0900

    一回渡す

--- test_net/Assets/User/Yamamoto/Script/player.cs
+++ test_net/Assets/User/Yamamoto/Script/player.cs
@@ -32,11 +32,11 @@ public class player : MonoBehaviourPunCallbacks
         test_net.Enable();
 
         // デバイス一覧を取得
-        //foreach (var device in InputSystem.devices)
-        //{
-        //    // デバイス名をログ出力
-        //    Debug.Log(device.name);
-        //}
+        foreach (var device in InputSystem.devices)
+        {
+            // デバイス名をログ出力
+            Debug.Log(device.name);
+        }
 
     }
     void Update()
