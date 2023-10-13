0c052dfd111f99fc91fdb172731b4ed650c6477a
MSI\GA4 <h1202016006@hiratagakuen.onmicrosoft.com>
2023-10-13 13:32:16 +0900

    連続ジャンプできないようにした

--- test_net/Assets/User/Yamamoto/PlayerInput/test_net.cs
+++ test_net/Assets/User/Yamamoto/PlayerInput/test_net.cs
@@ -37,24 +37,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
                     ""initialStateCheck"": true
                 },
                 {
-                    ""name"": ""Look"",
-                    ""type"": ""Value"",
-                    ""id"": ""335bd95f-018e-4ac7-937b-2aa2d0540e3c"",
-                    ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": true
-                },
-                {
-                    ""name"": ""Fire"",
-                    ""type"": ""Button"",
-                    ""id"": ""ba202da7-eafe-4d54-8085-66404c58e4da"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                },
-                {
                     ""name"": ""Jump"",
                     ""type"": ""Button"",
                     ""id"": ""b350f6fb-e6a5-4a9b-b978-df2bbbe25939"",
@@ -92,94 +74,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
                 }
             ],
             ""bindings"": [
-                {
-                    ""name"": """",
-                    ""id"": ""c1f7a91b-d0fd-4a62-997e-7fb9b69bf235"",
-                    ""path"": ""<Gamepad>/rightStick"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Gamepad"",
-                    ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""8c8e490b-c610-4785-884f-f04217b23ca4"",
-                    ""path"": ""<Pointer>/delta"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse;Touch"",
-                    ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""3e5f5442-8668-4b27-a940-df99bad7e831"",
-                    ""path"": ""<Joystick>/{Hatswitch}"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Joystick"",
-                    ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""143bb1cd-cc10-4eca-a2f0-a3664166fe91"",
-                    ""path"": ""<Gamepad>/rightTrigger"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Gamepad"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""05f6913d-c316-48b2-a6bb-e225f14c7960"",
-                    ""path"": ""<Mouse>/leftButton"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""886e731e-7071-4ae4-95c0-e61739dad6fd"",
-                    ""path"": ""<Touchscreen>/primaryTouch/tap"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Touch"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""ee3d0cd2-254e-47a7-a8cb-bc94d9658c54"",
-                    ""path"": ""<Joystick>/trigger"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Joystick"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""8255d333-5683-4943-a58a-ccb207ff1dce"",
-                    ""path"": ""<XRController>/{PrimaryAction}"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""XR"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
                 {
                     ""name"": """",
                     ""id"": ""70b2db4f-45ba-467c-8e84-fed8c95ae9e6"",
@@ -820,8 +714,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
-        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
-        m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         m_Player_Action = m_Player.FindAction("Action", throwIfNotFound: true);
         m_Player_ActionPress = m_Player.FindAction("ActionPress", throwIfNotFound: true);
@@ -900,8 +792,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
-    private readonly InputAction m_Player_Look;
-    private readonly InputAction m_Player_Fire;
     private readonly InputAction m_Player_Jump;
     private readonly InputAction m_Player_Action;
     private readonly InputAction m_Player_ActionPress;
@@ -911,8 +801,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
         private @Test_net m_Wrapper;
         public PlayerActions(@Test_net wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
-        public InputAction @Look => m_Wrapper.m_Player_Look;
-        public InputAction @Fire => m_Wrapper.m_Player_Fire;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputAction @Action => m_Wrapper.m_Player_Action;
         public InputAction @ActionPress => m_Wrapper.m_Player_ActionPress;
@@ -929,12 +817,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
             @Move.started += instance.OnMove;
             @Move.performed += instance.OnMove;
             @Move.canceled += instance.OnMove;
-            @Look.started += instance.OnLook;
-            @Look.performed += instance.OnLook;
-            @Look.canceled += instance.OnLook;
-            @Fire.started += instance.OnFire;
-            @Fire.performed += instance.OnFire;
-            @Fire.canceled += instance.OnFire;
             @Jump.started += instance.OnJump;
             @Jump.performed += instance.OnJump;
             @Jump.canceled += instance.OnJump;
@@ -954,12 +836,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
             @Move.started -= instance.OnMove;
             @Move.performed -= instance.OnMove;
             @Move.canceled -= instance.OnMove;
-            @Look.started -= instance.OnLook;
-            @Look.performed -= instance.OnLook;
-            @Look.canceled -= instance.OnLook;
-            @Fire.started -= instance.OnFire;
-            @Fire.performed -= instance.OnFire;
-            @Fire.canceled -= instance.OnFire;
             @Jump.started -= instance.OnJump;
             @Jump.performed -= instance.OnJump;
             @Jump.canceled -= instance.OnJump;
@@ -1155,8 +1031,6 @@ public partial class @Test_net: IInputActionCollection2, IDisposable
     public interface IPlayerActions
     {
         void OnMove(InputAction.CallbackContext context);
-        void OnLook(InputAction.CallbackContext context);
-        void OnFire(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
         void OnAction(InputAction.CallbackContext context);
         void OnActionPress(InputAction.CallbackContext context);
--- test_net/Assets/User/Yamamoto/Script/PlayerController.cs
+++ test_net/Assets/User/Yamamoto/Script/PlayerController.cs
@@ -4,17 +4,114 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    [SerializeField, Header("移動速度")]
+    private float moveSpeed;
+
+    [SerializeField, Header("ジャンプ速度")]
+    private float jumpSpeed;
+
+    //入力された方向を入れる変数
+    private Vector2 inputDirection;
+
+    //移動方向入れる変数
+    private Rigidbody2D rigid;
+
+    private bool bjump;//連続でジャンプさせないフラグ
+
+    private Test_net test_net;//inputsystemをスクリプトで呼び出す
+
     void Start()
     {
-        
-    }
+        //PlayerのRigidbody2Dコンポーネントを取得する
+        rigid = GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
+        //名前とIDを設定
+        gameObject.name = "Player" + photonView.OwnerActorNr;
+
+        test_net = new Test_net();//スクリプトを変数に格納
+        test_net.Enable();
+
+        // デバイス一覧を取得
+        foreach (var device in InputSystem.devices)
+        {
+            // デバイス名をログ出力
+            Debug.Log(device.name);
+        }
+
+    }
     void Update()
     {
         
+        //操作が競合しないための設定
+        if (photonView.IsMine)
+        {
+
+            Move();//移動処理をON
+        }
+    }
+
+    private void Move()//移動処理（計算部分）
+    {
+        //プレイヤーが入力した方向に横方向限定で移動速度分の力を加える
+        rigid.velocity = new Vector2(inputDirection.x * moveSpeed, rigid.velocity.y);
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (gameObject.name == "Player2")
+        {
+            Debug.Log("プレイヤー2認識");
+        }
+
+
+        //操作が競合しないための設定
+        if (photonView.IsMine)
+        {
+            Debug.Log("スティック動かして移動している");
+            //移動方向の入力情報がInputdirectionの中に入るようになる
+            inputDirection = context.ReadValue<Vector2>();
+        }
+        else
+        {
+            Debug.Log("識別できてない");
+        }
+    }
+
+    public void Onjump(InputAction.CallbackContext context)
+    {
+        //Input Systemからジャンプの入力があった時に呼ばれる
+        if (!context.performed || bjump)
+        {
+            return;
+        }
+
+        //操作が競合しないための設定
+        if (photonView.IsMine)
+        {
+            rigid.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            bjump = true;//一度ジャンプしたら着地するまでジャンプできなくする
+        }
+    }
+
+    public void OnAction(InputAction.CallbackContext context)
+    {
+        //操作が競合しないための設定
+        if (photonView.IsMine)
+        {
+            Debug.Log("アクション");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //プレイヤーが床または着地出来るものに乗っている時、再ジャンプ可能にする
+        if (collision.gameObject.tag == "Floor")
+        {
+            bjump = false;
+
+        }
     }
 }
