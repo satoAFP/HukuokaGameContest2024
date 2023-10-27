9ef379446c36ec99eb5c6fda27c255478b56ff19
MSI\GA4 <h1202019007@hiratagakuen.onmicrosoft.com>
2023-10-27 12:20:48 +0900

    リザルトテスト

--- test_net/Assets/User/Sato/Script/Manager/SceneMoveManager.cs
+++ test_net/Assets/User/Sato/Script/Manager/SceneMoveManager.cs
@@ -19,12 +19,26 @@ public class SceneMoveManager : MonoBehaviour
         }
     }
 
-    //コルーチン呼び出し用
+    /// <summary>
+    /// 引数の名前のシーンへ移動
+    /// </summary>
+    /// <param name="name">移動するシーン名</param>
     public void SceneMoveName(string name)
     {
         if (PhotonNetwork.IsMasterClient)
         {
             PhotonNetwork.LoadLevel(name);
+        }
+    }
+
+    /// <summary>
+    /// リトライ用
+    /// </summary>
+    public void SceneMoveRetry()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().name);
         }
     }
 }
