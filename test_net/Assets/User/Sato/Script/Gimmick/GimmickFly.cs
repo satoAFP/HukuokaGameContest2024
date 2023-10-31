01075576e036a7ead84c0bde40c95b1832faad3c
MSI\GA4 <h1202019007@hiratagakuen.onmicrosoft.com>
2023-10-31 16:00:31 +0900

    +

--- test_net/Assets/User/Sato/Script/Gimmick/GimmickFly.cs
+++ test_net/Assets/User/Sato/Script/Gimmick/GimmickFly.cs
@@ -11,17 +11,21 @@ public class GimmickFly : MonoBehaviourPunCallbacks
     [SerializeField, Header("移動量")]
     private float MovePower;
 
-    private DataManager dataManager;
+    [SerializeField, Header("回転量")]
+    private int MoveAngle;
 
-    private int ownerTapNum = 0;
+    private DataManager dataManager;        //データマネージャー
+
+    private int ownerTapNum = 0;            //それぞれのタップ回数
     private int clientTapNum = 0;
 
-    private int ownerCoolTimeCount = 0;
+    private int ownerCoolTimeCount = 0;     //それぞれのタップをやめた後のクールタイム
     private int clientCoolTimeCount = 0;
 
-    private bool isOwnerCoolTime = false;
+    private bool isOwnerCoolTime = false;   //それぞれクールタイム中か
     private bool isClientCoolTime = false;
 
+    //連続で反応しない
     private bool ownerFirst = true;
     private bool clientFirst = true;
     private bool OwnerCoolTimeFirst = true;
@@ -34,15 +38,17 @@ public class GimmickFly : MonoBehaviourPunCallbacks
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //データマネージャー取得
         dataManager = ManagerAccessor.Instance.dataManager;
 
+        //それぞれの連打処理
         if (dataManager.isOwnerInputKey_CB)
         {
             if (ownerFirst)
             {
-                ownerTapNum++;
+                ownerTapNum += MoveAngle;
                 ownerFirst = false;
                 ownerCoolTimeCount = 0;
             }
@@ -57,7 +63,7 @@ public class GimmickFly : MonoBehaviourPunCallbacks
         {
             if (clientFirst)
             {
-                clientTapNum++;
+                clientTapNum += MoveAngle;
                 clientFirst = false;
                 clientCoolTimeCount = 0;
             }
@@ -68,11 +74,13 @@ public class GimmickFly : MonoBehaviourPunCallbacks
             clientCoolTimeCount++;
         }
 
-
+        //タップ回数の差
         int dis = ownerTapNum - clientTapNum;
+        //移動量の倍率
         float mag = 0;
 
-        if(ownerCoolTimeCount>=CoolTime)
+        //クールタイム内に連打しないと落ちる
+        if (ownerCoolTimeCount >= CoolTime) 
         {
             if (OwnerCoolTimeFirst)
             {
@@ -106,6 +114,7 @@ public class GimmickFly : MonoBehaviourPunCallbacks
             }
         }
 
+        //倍率設定
         if (!isOwnerCoolTime && !isClientCoolTime) 
             mag = 2;
         else if (!isOwnerCoolTime)
@@ -115,15 +124,22 @@ public class GimmickFly : MonoBehaviourPunCallbacks
         else
             mag = 0;
 
+        //角度設定
         float rad = dis * Mathf.Deg2Rad; //角度をラジアン角に変換
 
+        //移動方向設定
         Vector2 power = new Vector2(Mathf.Sin(rad) * mag, Mathf.Cos(rad) * mag);
         Vector2 input;
         input.x = transform.position.x + power.x * MovePower;
         input.y = transform.position.y + power.y * MovePower;
 
-        Debug.Log(power);
+        if(isOwnerCoolTime && isClientCoolTime)
+        {
+            input.x = 0.0f;
+            input.y = 0.005f;
+        }
 
+        //移動量、角度の代入
         transform.position = input;
         transform.eulerAngles = new Vector3(0, 0, -dis);
     }
