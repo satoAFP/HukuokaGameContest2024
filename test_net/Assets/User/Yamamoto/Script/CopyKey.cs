07c0178e180be47e74f3b1b6402c02d3b1e2e9d4
MSI\GA4 <h1202016006@hiratagakuen.onmicrosoft.com>
2023-10-31 12:08:30 +0900

    key

--- test_net/Assets/User/Yamamoto/Script/CopyKey.cs
+++ test_net/Assets/User/Yamamoto/Script/CopyKey.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.InputSystem;
 
-public class CopyKey : MonoBehaviour
+public class CopyKey : MonoBehaviourPunCallbacks
 {
+    [SerializeField, Header("移動速度")]
+    private float moveSpeed;
+
+    [SerializeField, Header("ジャンプ速度")]
+    private float jumpSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
