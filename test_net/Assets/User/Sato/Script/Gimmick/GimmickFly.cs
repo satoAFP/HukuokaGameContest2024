476392b2122660a23f6f890249bd5e21baa95446
MSI\GA4 <h1202019007@hiratagakuen.onmicrosoft.com>
2023-10-31 16:28:00 +0900

    +

--- test_net/Assets/User/Sato/Script/Gimmick/GimmickFly.cs
+++ test_net/Assets/User/Sato/Script/Gimmick/GimmickFly.cs
@@ -16,6 +16,8 @@ public class GimmickFly : MonoBehaviourPunCallbacks
 
     private DataManager dataManager;        //データマネージャー
 
+    private bool isStart = false;           //ロケット開始
+
     private int ownerTapNum = 0;            //それぞれのタップ回数
     private int clientTapNum = 0;
 
@@ -43,105 +45,117 @@ public class GimmickFly : MonoBehaviourPunCallbacks
         //データマネージャー取得
         dataManager = ManagerAccessor.Instance.dataManager;
 
-        //それぞれの連打処理
         if (dataManager.isOwnerInputKey_CB)
         {
-            if (ownerFirst)
+            isStart = true;
+        }
+
+        if (isStart)
+        {
+            //それぞれの連打処理
+            if (dataManager.isOwnerInputKey_CB)
             {
-                ownerTapNum += MoveAngle;
-                ownerFirst = false;
-                ownerCoolTimeCount = 0;
+                if (ownerFirst)
+                {
+                    ownerTapNum += MoveAngle;
+                    ownerFirst = false;
+                    ownerCoolTimeCount = 0;
+                }
             }
-        }
-        else
-        {
-            ownerFirst = true;
-            ownerCoolTimeCount++;
-        }
-
-        if (dataManager.isClientInputKey_CB)
-        {
-            if (clientFirst)
+            else
             {
-                clientTapNum += MoveAngle;
-                clientFirst = false;
-                clientCoolTimeCount = 0;
+                ownerFirst = true;
+                ownerCoolTimeCount++;
             }
-        }
-        else
-        {
-            clientFirst = true;
-            clientCoolTimeCount++;
-        }
 
-        //タップ回数の差
-        int dis = ownerTapNum - clientTapNum;
-        //移動量の倍率
-        float mag = 0;
-
-        //クールタイム内に連打しないと落ちる
-        if (ownerCoolTimeCount >= CoolTime) 
-        {
-            if (OwnerCoolTimeFirst)
+            if (dataManager.isClientInputKey_CB)
             {
-                photonView.RPC(nameof(RpcShareIsOwnerCoolTime), RpcTarget.All, true);
-                ClientCoolTimeFirst = false;
+                if (clientFirst)
+                {
+                    clientTapNum += MoveAngle;
+                    clientFirst = false;
+                    clientCoolTimeCount = 0;
+                    Debug.Log("bbb");
+                }
             }
-        }
-        else
-        {
-            if (!OwnerCoolTimeFirst)
+            else
             {
-                photonView.RPC(nameof(RpcShareIsOwnerCoolTime), RpcTarget.All, false);
-                ClientCoolTimeFirst = true;
+                clientFirst = true;
+                clientCoolTimeCount++;
             }
-        }
 
-        if (clientCoolTimeCount >= CoolTime)
-        {
-            if (ClientCoolTimeFirst)
+            //タップ回数の差
+            int dis = ownerTapNum - clientTapNum;
+            //移動量の倍率
+            float mag = 0;
+
+            //クールタイム内に連打しないと落ちる
+            if (ownerCoolTimeCount >= CoolTime)
             {
-                photonView.RPC(nameof(RpcShareIsClientCoolTime), RpcTarget.All, true);
-                ClientCoolTimeFirst = false;
+                if (OwnerCoolTimeFirst)
+                {
+                    photonView.RPC(nameof(RpcShareIsOwnerCoolTime), RpcTarget.All, true);
+                    OwnerCoolTimeFirst = false;
+                }
             }
-        }
-        else
-        {
-            if (!OwnerCoolTimeFirst)
+            else
             {
-                photonView.RPC(nameof(RpcShareIsClientCoolTime), RpcTarget.All, false);
-                ClientCoolTimeFirst = true;
+                if (!OwnerCoolTimeFirst)
+                {
+                    photonView.RPC(nameof(RpcShareIsOwnerCoolTime), RpcTarget.All, false);
+                    OwnerCoolTimeFirst = true;
+                }
             }
+
+            if (clientCoolTimeCount >= CoolTime)
+            {
+                if (ClientCoolTimeFirst)
+                {
+                    photonView.RPC(nameof(RpcShareIsClientCoolTime), RpcTarget.All, true);
+                    ClientCoolTimeFirst = false;
+                }
+            }
+            else
+            {
+                if (!ClientCoolTimeFirst)
+                {
+                    photonView.RPC(nameof(RpcShareIsClientCoolTime), RpcTarget.All, false);
+                    ClientCoolTimeFirst = true;
+                    Debug.Log("ccc");
+                }
+            }
+
+            //倍率設定
+            if (!isOwnerCoolTime && !isClientCoolTime)
+                mag = 2;
+            else if (!isOwnerCoolTime)
+                mag = 1;
+            else if (!isClientCoolTime)
+                mag = 1;
+            else
+                mag = 0;
+
+            //角度設定
+            float rad = dis * Mathf.Deg2Rad; //角度をラジアン角に変換
+
+            Debug.Log(isOwnerCoolTime+":"+ isClientCoolTime);
+
+            //移動方向設定
+            Vector2 power = new Vector2(Mathf.Sin(rad) * mag, Mathf.Cos(rad) * mag);
+            Vector2 input;
+            input.x = transform.position.x + power.x * MovePower;
+            input.y = transform.position.y + power.y * MovePower;
+
+            if (isOwnerCoolTime && isClientCoolTime)
+            {
+                input.y = transform.position.y - 0.005f;
+                Debug.Log("aaa");
+            }
+
+            //移動量、角度の代入
+            transform.position = input;
+            transform.eulerAngles = new Vector3(0, 0, -dis);
         }
-
-        //倍率設定
-        if (!isOwnerCoolTime && !isClientCoolTime) 
-            mag = 2;
-        else if (!isOwnerCoolTime)
-            mag = 1;
-        else if (!isClientCoolTime)
-            mag = 1;
-        else
-            mag = 0;
-
-        //角度設定
-        float rad = dis * Mathf.Deg2Rad; //角度をラジアン角に変換
-
-        //移動方向設定
-        Vector2 power = new Vector2(Mathf.Sin(rad) * mag, Mathf.Cos(rad) * mag);
-        Vector2 input;
-        input.x = transform.position.x + power.x * MovePower;
-        input.y = transform.position.y + power.y * MovePower;
-
-        if(isOwnerCoolTime && isClientCoolTime)
-        {
-            input.x = 0.0f;
-            input.y = 0.005f;
-        }
-
-        //移動量、角度の代入
-        transform.position = input;
-        transform.eulerAngles = new Vector3(0, 0, -dis);
     }
 
 
