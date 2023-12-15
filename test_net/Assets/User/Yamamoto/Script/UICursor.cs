f7299f46e1b675be225b8a4184ccb2a1028821da
yamakan-z <h1202016006@hiratagakuen.onmicrosoft.com>
2023-12-15 15:48:55 +0900

    点滅処理書き換え

--- test_net/Assets/User/Yamamoto/Script/UICursor.cs
+++ test_net/Assets/User/Yamamoto/Script/UICursor.cs
@@ -21,8 +21,8 @@ public class UICursor : MonoBehaviourPunCallbacks
 
     private bool movestart = false;//移動中かを判断する
 
-    // 点滅させる対象
-    [SerializeField] private Renderer _target;
+    // 点滅させる対象（ここがBehaviourに変更されている）
+    [SerializeField] private Behaviour _target;
     // 点滅周期[s]
     [SerializeField] private float _cycle = 1;
 
@@ -32,8 +32,6 @@ public class UICursor : MonoBehaviourPunCallbacks
     void Start()
     {
         test_net = new Test_net();//スクリプトを変数に格納
-
-        _target = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -41,20 +39,23 @@ public class UICursor : MonoBehaviourPunCallbacks
     {
         DataManager datamanager = ManagerAccessor.Instance.dataManager;
 
-        // 内部時刻を経過させる
-        _time += Time.deltaTime;
-
-        if (ManagerAccessor.Instance.dataManager.player1 != null)
+        if(ManagerAccessor.Instance.dataManager.player1 != null)
         {
             //箱を開けている時カーソル移動をする
             if (!ManagerAccessor.Instance.dataManager.player1.GetComponent<PlayerController>().cursorlock)
             {
+                // 内部時刻を経過させる
+                _time += Time.deltaTime;
+
                 // 周期cycleで繰り返す値の取得
                 // 0～cycleの範囲の値が得られる
                 var repeatValue = Mathf.Repeat((float)_time, _cycle);
 
                 // 内部時刻timeにおける明滅状態を反映
                 _target.enabled = repeatValue >= _cycle * 0.5f;
+
+                //画像を半透明にする
+                GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 128);
 
                 //押されたボタンの左右でカーソルの移動位置を決める
                 if (datamanager.isOwnerInputKey_C_D_RIGHT && !movestart)
