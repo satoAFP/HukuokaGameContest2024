3114e246cccc9610c167b41d9439636e15bdab7b
satoAFP <72423443+satoAFP@users.noreply.github.com>
2023-12-19 10:20:52 +0900

    リザルト一応できた

--- test_net/Assets/User/Sato/Script/System/ButtonControlSystem.cs
+++ test_net/Assets/User/Sato/Script/System/ButtonControlSystem.cs
@@ -11,8 +11,13 @@ public class ButtonControlSystem : MonoBehaviourPunCallbacks
 
     [SerializeField, Header("縦＝true 横＝false")] private bool inputDirection;
 
-    private bool buttonNum = true;
+    private DataManager dataManager;        //データマネージャー取得用
 
+    private bool buttonNum = true;          //ボタンの数
+
+    private bool isSelect = false;          //ボタンが選択されたとき
+
+    //連続で入らないよう
     private bool first = true;
     private bool firstSceneMove = true;
 
@@ -20,69 +25,75 @@ public class ButtonControlSystem : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        if (PhotonNetwork.IsMasterClient)
+        if (!isSelect)
         {
-            if (inputDirection)
+            dataManager = ManagerAccessor.Instance.dataManager;
+
+            if (PhotonNetwork.IsMasterClient)
             {
-                //上下を入力するとカーソルが上下する
-                if (ManagerAccessor.Instance.dataManager.isOwnerInputKey_C_L_UP || ManagerAccessor.Instance.dataManager.isOwnerInputKey_C_L_UP)
+                if (inputDirection)
                 {
-                    if (first)
+                    //上下を入力するとカーソルが上下する
+                    if (dataManager.isOwnerInputKey_C_L_UP || dataManager.isOwnerInputKey_C_L_UP)
                     {
-                        buttonNum = !buttonNum;
-                        first = false;
+                        if (first)
+                        {
+                            buttonNum = !buttonNum;
+                            first = false;
+                        }
                     }
                     else
                         first = true;
                 }
-            }
-            else
-            {
-                //左右を入力するとカーソルが左右する
-                if (ManagerAccessor.Instance.dataManager.isOwnerInputKey_CA /*|| ManagerAccessor.Instance.dataManager.isOwnerInputKey_C_L_LEFT*/)
+                else
                 {
-                    if (first)
+                    //左右を入力するとカーソルが左右する
+                    if (dataManager.isOwnerInputKey_C_L_RIGHT || dataManager.isOwnerInputKey_C_L_LEFT)
                     {
-                        Debug.Log("aaa");
-                        buttonNum = !buttonNum;
-                        first = false;
+                        if (first)
+                        {
+                            buttonNum = !buttonNum;
+                            first = false;
+                        }
                     }
                     else
-                    {
-                        Debug.Log("bbb");
                         first = true;
-                    }
                 }
-            }
 
-            
-            if (buttonNum)
-            {
-                underber.GetComponent<RectTransform>().position = Buttons[0].GetComponent<RectTransform>().position;
-                underber.GetComponent<RectTransform>().sizeDelta = new Vector2(Buttons[0].GetComponent<RectTransform>().sizeDelta.x, underber.GetComponent<RectTransform>().sizeDelta.y);
-            }
-            else
-            {
-                underber.GetComponent<RectTransform>().position = Buttons[0].GetComponent<RectTransform>().position;
-                underber.GetComponent<RectTransform>().sizeDelta = new Vector2(Buttons[0].GetComponent<RectTransform>().sizeDelta.x, underber.GetComponent<RectTransform>().sizeDelta.y);
-            }
-
-
-
-            //シーン移動処理
-            if (ManagerAccessor.Instance.dataManager.isOwnerInputKey_B)
-            {
-                if (firstSceneMove)
+                //ボタンにあった座標とサイズに変更
+                if (buttonNum)
                 {
-                    if (buttonNum)
+                    RectTransform buttonTra = Buttons[0].GetComponent<RectTransform>();
+                    Debug.Log(buttonTra.sizeDelta.y / 2);
+                    underber.GetComponent<RectTransform>().position = new Vector2(buttonTra.position.x, buttonTra.position.y - buttonTra.sizeDelta.y / 2);
+                    underber.GetComponent<RectTransform>().sizeDelta = new Vector2(buttonTra.sizeDelta.x, underber.GetComponent<RectTransform>().sizeDelta.y);
+                }
+                else
+                {
+                    RectTransform buttonTra = Buttons[1].GetComponent<RectTransform>();
+                    underber.GetComponent<RectTransform>().position = new Vector2(buttonTra.position.x, buttonTra.position.y - buttonTra.sizeDelta.y / 2);
+                    underber.GetComponent<RectTransform>().sizeDelta = new Vector2(buttonTra.sizeDelta.x, underber.GetComponent<RectTransform>().sizeDelta.y);
+                }
+
+
+
+                //シーン移動処理
+                if (dataManager.isOwnerInputKey_CB)
+                {
+                    if (firstSceneMove)
                     {
-                        ManagerAccessor.Instance.sceneMoveManager.SceneMoveRetry();
-                        firstSceneMove = false;
-                    }
-                    else
-                    {
-                        ManagerAccessor.Instance.sceneMoveManager.SceneMoveName("StageSelect");
-                        firstSceneMove = false;
+                        if (buttonNum)
+                        {
+                            ManagerAccessor.Instance.sceneMoveManager.SceneMoveRetry();
+                            isSelect = true;
+                            firstSceneMove = false;
+                        }
+                        else
+                        {
+                            ManagerAccessor.Instance.sceneMoveManager.SceneMoveName("StageSelect");
+                            isSelect = true;
+                            firstSceneMove = false;
+                        }
                     }
                 }
             }
