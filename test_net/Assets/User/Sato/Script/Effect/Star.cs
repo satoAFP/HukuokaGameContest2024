5df8a75f7c244430a33c175ca59e677b83a3d23d
satoAFP <72423443+satoAFP@users.noreply.github.com>
2024-01-12 15:28:50 +0900

    星のエフェクト修正

--- test_net/Assets/User/Sato/Script/Effect/Star.cs
+++ test_net/Assets/User/Sato/Script/Effect/Star.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
+    [SerializeField,Header("à⁄ìÆó ")]private float[] movePower;
+
+    [SerializeField, Header("è¨Ç≥Ç≠Ç»ÇÈë¨ìx")] private Vector3 smallPower;
+
     private Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
     {
-        dir.x = Random.Range(-0.1f, 0.1f);
-        dir.y = Random.Range(-0.1f, 0.1f);
+        if (Random.Range(0, 2) == 0) 
+            dir.x = Random.Range(movePower[0], movePower[1]);
+        else
+            dir.x = -Random.Range(movePower[0], movePower[1]);
+
+        if (Random.Range(0, 2) == 0)
+            dir.y = Random.Range(movePower[0], movePower[1]);
+        else
+            dir.y = -Random.Range(movePower[0], movePower[1]);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += dir;
+        transform.localScale -= smallPower;
     }
 }
