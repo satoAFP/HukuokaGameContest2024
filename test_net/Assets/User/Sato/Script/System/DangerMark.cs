b0079c546548d04514a5d048e48ec12e6c5b80ed
MSI\GA4 <h1202019007@hiratagakuen.onmicrosoft.com>
2024-01-18 15:43:33 +0900

    +

--- test_net/Assets/User/Sato/Script/System/DangerMark.cs
+++ test_net/Assets/User/Sato/Script/System/DangerMark.cs
@@ -6,6 +6,7 @@ using Photon.Pun;
 public class DangerMark : MonoBehaviourPunCallbacks
 {
     [SerializeField, Header("落石")] private Transform stoneGenelate;
+    [SerializeField, Header("カメラ")] private Transform cameraGenelate;
 
     [SerializeField, Header("落石の前後どの座標に生成するか")] private float genelatePosX;
     [SerializeField, Header("プレイヤーの上下どの座標に生成するか")] private float genelatePosY;
@@ -17,10 +18,7 @@ public class DangerMark : MonoBehaviourPunCallbacks
         Vector3 pos = Vector3.zero;
 
         //表示する座標の設定
-        if (PhotonNetwork.IsMasterClient)
-            pos = new Vector3(stoneGenelate.position.x + genelatePosX, ManagerAccessor.Instance.dataManager.player1.transform.position.y + genelatePosY);
-        else
-            pos = new Vector3(stoneGenelate.position.x + genelatePosX, ManagerAccessor.Instance.dataManager.player2.transform.position.y + genelatePosY);
+        pos = new Vector3(stoneGenelate.position.x + genelatePosX, cameraGenelate.position.y + genelatePosY);
 
         //座標変更
         transform.position = pos;
