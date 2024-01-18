8f90b078fdf2ec865812e7c0b96f2c985ede5268
MSI\GA4 <h1202019007@hiratagakuen.onmicrosoft.com>
2024-01-18 15:10:45 +0900

    プレイヤー2がステージに入ることができなくなった

--- test_net/Assets/User/Sato/Script/System/StageSelect.cs
+++ test_net/Assets/User/Sato/Script/System/StageSelect.cs
@@ -41,8 +41,6 @@ public class StageSelect : MonoBehaviourPunCallbacks
         //ステージに入る
         if (PhotonNetwork.LocalPlayer.IsMasterClient)
             MoveStage(isOwnerEnter, ManagerAccessor.Instance.dataManager.isOwnerInputKey_C_D_UP);
-        else
-            MoveStage(isClientEnter, ManagerAccessor.Instance.dataManager.isClientInputKey_C_D_UP);
 
         //フェード処理
         if (isOwnerFadeStart)
@@ -66,25 +64,6 @@ public class StageSelect : MonoBehaviourPunCallbacks
             }
         }
 
-        if (isClientFadeStart)
-        {
-            //押すべきボタンの画像非表示
-            ManagerAccessor.Instance.dataManager.player2.transform.GetChild(0).gameObject.SetActive(false);
-            
-            if (ManagerAccessor.Instance.dataManager.player2.transform.Find("PlayerImage").GetComponent<SpriteRenderer>().color.a > 0)
-            { 
-                ManagerAccessor.Instance.dataManager.player2.transform.Find("PlayerImage").GetComponent<SpriteRenderer>().color -= new Color32(0, 0, 0, (byte)FeedSpeed);//SE再生
-                
-                SECount++;
-                if (SECount <= SEInterbal * (SEPlayNum - 1))
-                {
-                    if (SECount % SEInterbal == 0 || SECount == 1)
-                    {
-                        audioSource.PlayOneShot(EnterSE);
-                    }
-                }
-            }
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -102,18 +81,6 @@ public class StageSelect : MonoBehaviourPunCallbacks
             }
         }
 
-        if (collision.gameObject.name == "Player2")
-        {
-            if (!PhotonNetwork.IsMasterClient)
-            {
-                //押すべきボタンの画像表示
-                collision.transform.GetChild(0).gameObject.SetActive(true);
-                collision.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = ManagerAccessor.Instance.spriteManager.CrossUp;
-
-                photonView.RPC(nameof(RpcShareIsNotOpenBox), RpcTarget.All, false, true);
-                photonView.RPC(nameof(RpcShareIsClientEnter), RpcTarget.All, true);
-            }
-        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -128,15 +95,6 @@ public class StageSelect : MonoBehaviourPunCallbacks
 
         }
 
-        if (collision.gameObject.name == "Player2")
-        {
-            //押すべきボタンの画像非表示
-            collision.transform.GetChild(0).gameObject.SetActive(false);
-
-            photonView.RPC(nameof(RpcShareIsNotOpenBox), RpcTarget.All, false, false);
-            photonView.RPC(nameof(RpcShareIsClientEnter), RpcTarget.All, false);
-
-        }
     }
 
     //シーン切り替え情報共有
